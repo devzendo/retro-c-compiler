ddabd443e06b095a1a9a3c1ccf9e649b0aceec8e
Matt Gumbley <matt.gumbley@devzendo.org>
2024-05-30 18:52:16 +0100

    Remove unused code/imports.

--- crates/rcc/src/driver_spec.rs
+++ crates/rcc/src/driver_spec.rs
@@ -3,28 +3,9 @@ extern crate hamcrest2;
 #[cfg(test)]
 mod driver_spec {
 
-    use std::{fs::File, path::Path};
-
-    use hamcrest2::prelude::*;
-    use temp_testdir::TempDir;
-
-    use crate::{parse_command_line, validate_command_line};
-
     #[ctor::ctor]
     fn before_each() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
-
-
-    fn temp_config_dir() -> (Box<Path>, TempDir) {
-        // Return both objects as if temp_dir is not moved back to the caller, it'll drop and
-        // delete.
-        let temp_dir = TempDir::default();
-        let temp = temp_dir.to_path_buf();
-        assert_that!(temp.as_path(), dir_exists());
-
-        (temp.into_boxed_path(), temp_dir)
-    }
-
     
 }
