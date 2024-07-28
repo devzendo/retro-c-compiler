8f75603690fcfd28c26ee20e7666731d9e6d0593
Matt Gumbley <matt.gumbley@devzendo.org>
2024-07-28 21:12:17 +0100

    Oops, forgot to add.

---
+++ crates/rcc/src/file_utils_spec.rs
@@ -0,0 +1,15 @@
+
+use hamcrest2::prelude::*;
+use std::path::Path;
+use temp_testdir::TempDir;
+
+pub fn temp_config_dir() -> (Box<Path>, TempDir) {
+    // Return both objects as if temp_dir is not moved back to the caller, it'll drop and
+    // delete.
+
+    let temp_dir = TempDir::default();
+    let temp = temp_dir.to_path_buf();
+    assert_that!(temp.as_path(), dir_exists());
+
+    (temp.into_boxed_path(), temp_dir)
+}
